57e308c1903d1c9dc5f0f044b4665795dc822097
cgaspard3333 <clement.gaspard@u-bordeaux.fr>
2023-02-17 11:12:56 +0100

    Update trace_data to use all experiments to compute coeff of the model + Add new experiments of 30s

--- acquisition.py
+++ acquisition.py
@@ -4,7 +4,8 @@ import struct
 import sys
 import random
 
-ser = serial.Serial('/dev/ttyACM0',921600)  # open serial port 921600,8,N,1
+# ser = serial.Serial('/dev/ttyACM0', 576000)  # open serial port 921600,8,N,1
+ser = serial.Serial('/dev/ttyACM0', 921600)
 
 print(ser.name)         # check which port was really used
 
@@ -27,7 +28,7 @@ test = input()
 ser.write(b'exp\r')     # write a string
 print(ser.read(2)) # eliminate first '\r\n'
 
-for i in range (0,20001):
+for i in range (0,30001):
     # if i % 1000 == 0:
     #     currentvalue = round(random.uniform(-0.1,0.1),1)
     #     torque_head = "torque_cmd 1 "
@@ -38,14 +39,16 @@ for i in range (0,20001):
     if header == b'\xff':
         header2 = ser.read(1)
         if header2 == b'\xaa':
-            data = ser.read(10)
-            delta_t, current, pos = struct.unpack('<Ihi',data)
-            # print(delta_t, current, pos)
-            f.write(str(delta_t))
+            data = ser.read(11)
+            ts, current, pos, cmd_curr = struct.unpack('<Ihib',data)
+            # print(ts, current, pos)
+            f.write(str(ts))
             f.write(",")
             f.write(str(current))
             f.write(",")
             f.write(str(pos))
+            f.write(",")
+            f.write(str(cmd_curr))
             f.write("\n")
         
     # print(data)
--- trace_data.py
+++ trace_data.py
@@ -12,7 +12,17 @@ path = "dataset"
 # Change the directory
 os.chdir(path)
 
+step = 0
 g = 9.81 #m/s²
+A_multi  = np.array([[0,0,0]]) 
+A_multi = A_multi[:-1]
+delta_speed_multi = np.array([])
+
+def slider(value, array, size_sliding_windows):
+    index_l = np.searchsorted(array,value - size_sliding_windows)
+    index_h = np.searchsorted(array,value + size_sliding_windows,'right')
+    array_slide = array[index_l:index_h]
+    return array_slide, index_l, index_h
 
 # iterate through all file
 for file in os.listdir():
@@ -26,18 +36,12 @@ for file in os.listdir():
         ts=np.array([])
         torque=np.array([])
         pos=np.array([])
+        torque_cmd=np.array([])
 
         A_part2 = np.array([[0, 0],
                             [0, 0]]) 
         B = np.array([]) 
-
-        def slider(value, array, size_sliding_windows):
-            index_l = np.searchsorted(array,value - size_sliding_windows)
-            index_h = np.searchsorted(array,value + size_sliding_windows,'right')
-            array_slide = array[index_l:index_h]
-            return array_slide, index_l, index_h
-            
-
+        
         while True:
             count += 1
             line = f.readline()
@@ -50,103 +54,111 @@ for file in os.listdir():
             ts = np.append(ts,int(new_str[0])/1e6)
             torque = np.append(torque,int(new_str[1]) * 33/2048)
             pos = np.append(pos,(int(new_str[2])-52706) * 2*math.pi/65535/6)
+            torque_cmd = np.append(torque_cmd,int(new_str[3]) * 32/2000)
 
-            # test = -math.sin(int(new_str[2]) * 360/65535/6)*g
-            # A_part2 = np.append(A_part2,np.array([[int(new_str[1]) * 33/2048,test]]), axis=0)
-
-        # A_part2 = np.concatenate((ts.T,torque))
-
-        # print(A_part2)
-
-        size_sliding_windows = 0.01 #s =10ms
-        sample_rate = 0.01 #s =10ms
+        size_sliding_windows = 0.05 #s =10ms
+        sample_rate = 0.003 #s =10ms
 
         resampled_ts=np.array([])
         speed=np.array([])
         torque_lstsq=np.array([])
         pos_lstsq=np.array([])
-
-
+        torque_cmd_lstsq=np.array([])
 
         for i in np.arange(int(ts[0]),int(ts[-1]),sample_rate):
             ts_window, index_l, index_h = slider(i, ts, size_sliding_windows)
             pos_slide = pos[index_l:index_h]
             torque_slide = torque[index_l:index_h]
+            torque_cmd_slide = torque_cmd[index_l:index_h]
             
             A = np.vstack([ts_window, np.ones(len(ts_window))]).T
             pos_a, pos_b = np.linalg.lstsq(A, pos_slide, rcond=None)[0]
             torque_a, torque_b = np.linalg.lstsq(A, torque_slide, rcond=None)[0]
+            torque_cmd_a, torque_cmd_b = np.linalg.lstsq(A, torque_cmd_slide, rcond=None)[0]
 
             speed = np.append(speed, pos_a)
             resampled_ts = np.append(resampled_ts, i)
             torque_lstsq = np.append(torque_lstsq, torque_a*i+torque_b)
             pos_lstsq = np.append(pos_lstsq, pos_a*i+pos_b)
+            torque_cmd_lstsq = np.append(torque_cmd_lstsq, torque_cmd_a*i+torque_cmd_b)
 
         resampled_ts = resampled_ts[:-1]
         torque_lstsq = torque_lstsq[:-1]
         pos_lstsq = pos_lstsq[:-1]
+        torque_cmd_lstsq = torque_cmd_lstsq[:-1]
 
-        A_bis = sample_rate * np.vstack([torque_lstsq, -np.sin(pos_lstsq)*g]).T
         speed_new = speed[1:]
         speed = speed[:-1]
+        speed_sign = np.sign(speed * (abs(speed) > 0.01))
+        A_bis = sample_rate * np.vstack([torque_cmd_lstsq, -np.sin(pos_lstsq)*g, speed_sign]).T
         delta_speed = speed_new - speed
+    
+        delta_speed_multi = np.append(delta_speed_multi, delta_speed)
+        A_multi = np.concatenate((A_multi, A_bis), axis=0)
 
-        # print(speed, "\n")
-        # print(speed_new, "\n")
-        # print(delta_speed, "\n")
+    step = step + 1
+    print("Step",step,"/",len(os.listdir()))
 
-        alpha, beta = np.linalg.lstsq(A_bis, delta_speed, rcond=None)[0]
+coeffs = np.linalg.lstsq(A_multi, delta_speed_multi, rcond=None)[0]
 
-        delta_speed_sim = A_bis @ [alpha, beta]
+delta_speed_sim = A_bis @ coeffs
 
-        states = [
-            [pos_lstsq[0], speed[0]]
-        ]
-        for step in range(len(speed)):
-            state = states[-1]
-            _delta_speed = alpha * torque_lstsq[step] - beta * np.sin(state[0]) * g
-            new_speed = state[1] + sample_rate * _delta_speed
-            new_pos = state[0] + sample_rate * new_speed
-            states.append([new_pos, new_speed])
+states = [
+    [pos_lstsq[0], speed[0]]
+]
+for step in range(len(speed)):
+    state = states[-1]
+    speed_sign = 0.
+    if abs(state[1]) > 0.1:
+        speed_sign = np.sign(state[1])
+    _delta_speed = coeffs[0] * torque_cmd_lstsq[step] - coeffs[1] * np.sin(state[0]) * g + coeffs[2] * speed_sign
+    new_speed = state[1] + sample_rate * _delta_speed
+    new_pos = state[0] + sample_rate * new_speed
+    states.append([new_pos, new_speed])
 
-        states = np.array(states)
+states = np.array(states)
 
-        print(file, alpha, beta, 1/beta)
+print(coeffs)
+print("Alpha :", coeffs[0], "| Beta :", coeffs[1], "| l:", 1/coeffs[1])
 
-        fig, axs = plt.subplots(6)
-        
-        axs[0].plot(ts, pos)
-        axs[0].set_xlabel('Time (s)')
-        axs[0].set_ylabel('Position (°)')
+fig, axs = plt.subplots(5, sharex=True)
 
-        axs[1].plot(resampled_ts, pos_lstsq, color="red") 
-        axs[1].set_xlabel('Time (s)')
-        axs[1].set_ylabel('Position resampled (°)')
+# axs[0].plot(ts, pos)
+# axs[0].set_xlabel('Time (s)')
+# axs[0].set_ylabel('Position (rad)')
 
-        axs[2].plot(resampled_ts, speed, color="green") 
-        axs[2].set_xlabel('Time (s)')
-        axs[2].set_ylabel('Speed (°/s)')
+axs[0].plot(resampled_ts, pos_lstsq, color="red") 
+axs[0].set_xlabel('Time (s)')
+axs[0].set_ylabel('Position resampled (rad)')
 
-        axs[3].plot(resampled_ts, torque_lstsq, color="orange") 
-        axs[3].set_xlabel('Time (s)')
-        axs[3].set_ylabel('Current (A)')
+axs[1].plot(resampled_ts, speed, color="green") 
+axs[1].set_xlabel('Time (s)')
+axs[1].set_ylabel('Speed (rad/s)')
 
-        axs[4].plot(resampled_ts, delta_speed, color="blue") 
-        axs[4].set_xlabel('Time (s)')
-        axs[4].set_ylabel('Delta speed')
+axs[2].plot(resampled_ts, torque_lstsq, color="orange") 
+axs[2].set_xlabel('Time (s)')
+axs[2].set_ylabel('Sim Current (A)')
 
-        axs[4].plot(resampled_ts, delta_speed_sim, color="orange") 
-        axs[4].set_xlabel('Time (s)')
-        axs[4].set_ylabel('Sim delta speed')
+axs[2].plot(resampled_ts, torque_cmd_lstsq, color="green") 
+axs[2].set_xlabel('Time (s)')
+axs[2].set_ylabel('Current (A)')
 
-        axs[5].plot(resampled_ts, pos_lstsq, color="blue") 
-        axs[5].set_xlabel('Time (s)')
-        axs[5].set_ylabel('Real pos')
+axs[3].plot(resampled_ts, delta_speed, color="blue") 
+axs[3].set_xlabel('Time (s)')
+axs[3].set_ylabel('Delta speed')
 
-        axs[5].plot(resampled_ts, states.T[0][1:], color="orange") 
-        axs[5].set_xlabel('Time (s)')
-        axs[5].set_ylabel('Sim pos')
+axs[3].plot(resampled_ts, delta_speed_sim, color="orange") 
+axs[3].set_xlabel('Time (s)')
+axs[3].set_ylabel('Sim delta speed')
 
-        fig.suptitle('Position, Speed and Current of RMD-X6 with random current commands (+/-0.73A)', fontsize=16)
+axs[4].plot(resampled_ts, pos_lstsq, color="blue") 
+axs[4].set_xlabel('Time (s)')
+axs[4].set_ylabel('Real pos')
 
-        plt.show()
+axs[4].plot(resampled_ts, states.T[0][1:], color="orange") 
+axs[4].set_xlabel('Time (s)')
+axs[4].set_ylabel('Sim pos')
+
+fig.suptitle('Position, Speed and Current of RMD-X6 with random current commands (+/-0.73A)', fontsize=16)
+
+plt.show()
